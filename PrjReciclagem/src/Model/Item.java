68f885e178f3f23c70376340eeb5b04868244825
Kzoy <fredcabrera@outlook.com>
2023-11-16 22:46:03 -0300

    alteracao item

--- PrjReciclagem/src/Model/Item.java
+++ PrjReciclagem/src/Model/Item.java
@@ -1,28 +1,55 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ 
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+*/
 package Model;
 
 /**
  *
- * @author Frederico
- */
+ 
+@author Frederico*/
 public class Item {
     // Propriedades
     private int idItem;
     private String descricao;
     private float valorUnitario;
+    private String unidade;
 
     //Construtores
-    public Item(int iditem, String descricao, float valorunitario) {
+    public Item(int iditem, String descricao, float valorunitario, String unidade) {
         this.idItem = iditem;
         this.descricao = descricao;
         this.valorUnitario = valorunitario;
+        this.unidade = unidade;
     }
-    
+
+    public int getIdItem() {
+        return idItem;
+    }
+
+    public void setIdItem(int idItem) {
+        this.idItem = idItem;
+    }
+
+    public float getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(float valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
     //metodos publicos
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
     public String getDescricao() {
         return descricao;
     }
@@ -31,12 +58,4 @@ public class Item {
         this.descricao = descricao;
     }
 
-    public float getValorunitario() {
-        return valorUnitario;
-    }
-
-    public void setValorunitario(float valorunitario) {
-        this.valorUnitario = valorunitario;
-    }
-    
 }
